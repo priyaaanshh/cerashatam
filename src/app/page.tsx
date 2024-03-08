5d014b163684aa476acfc64b8e622bef345e29e8
thepriyansh01 <priyansh.m.2002@gmail.com>
2024-03-08 17:20:00 +0530

    added skeleton on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Skeleton } from "@/components/ui/skeleton"
 
 import Items from '@/components/custom/dashboard_content/corousel_Items';
 
@@ -28,7 +29,16 @@ const Home = () => {
             <CarouselNext className='hover:bg-primary hover:dark:text-secondary' />
           </div>
         </Carousel>
-
+      </div>
+      <div className="flex justify-center items-center w-full my-10">
+        <div className="flex flex-col justify-center items-start gap-5 w-full max-w-[1440px] px-4">
+          <div className="flex justify-center items-center w-full">
+            <Skeleton className="size-32 rounded-full bg-slate-500" />
+          </div>
+          <Skeleton className="w-full h-64 rounded-xl bg-slate-500" />
+          <Skeleton className="w-full max-w-[1080px] h-10 rounded-xl bg-slate-500" />
+          <Skeleton className="w-full max-w-96 h-10 rounded-xl bg-slate-500" />
+        </div>
       </div>
     </div>
   )
